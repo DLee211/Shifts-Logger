9311e73c037e74a1e6d824ac09323ad29ea418e2
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-02-06 23:49:02 -0500

    Added menu

---
+++ Shifts Logger/Enums.cs
@@ -0,0 +1,29 @@
+﻿namespace Shifts_Logger;
+
+public class Enums
+{
+    internal enum MainMenuOptions
+    {
+       ManageShifts,
+       ManageWorkers,
+       Quit
+    }
+    
+    internal enum ShiftMenuOptions
+    {
+        GetShifts,
+        GetShift,
+        AddShifts,
+        DeleteShifts,
+        UpdateShifts,
+        Quit
+    }
+    
+    internal enum WorkerMenuOptions
+    {
+        AddWorker,
+        DeleteWorker,
+        UpdateWorker,
+        Quit
+    }
+}
---
+++ Shifts Logger/UserInterface.cs
@@ -0,0 +1,40 @@
+﻿using Spectre.Console;
+
+namespace Shifts_Logger;
+
+public class UserInterface
+{
+    public void MainMenu()
+    {
+        var isAppRunning = true;
+        while (isAppRunning)
+        {
+            Console.Clear();
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<Enums.MainMenuOptions>()
+                    .Title("Shift Logger Main Menu")
+                    .AddChoices(
+                        Enums.MainMenuOptions.ManageShifts,
+                        Enums.MainMenuOptions.ManageWorkers,
+                        Enums.MainMenuOptions.Quit
+                    ));
+
+            switch (option)
+            {
+                case Enums.MainMenuOptions.ManageShifts:
+                    Console.Clear();
+                    break;
+                
+                case Enums.MainMenuOptions.ManageWorkers:
+                    Console.Clear();
+                    break;
+                
+                case Enums.MainMenuOptions.Quit:
+                    Console.Clear();
+                    Console.WriteLine("Goodbye!");
+                    isAppRunning = false;
+                    break;
+            }
+        }
+    }
+}
